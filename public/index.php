7fdd44d8031e7a7c6cf4fe4079f65834f0f4703c
Martin Vach <martin.vach@mynxg.com>
2021-01-27 12:28:39 +0100

    U

--- public/index.php
+++ public/index.php
@@ -7,11 +7,15 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$middleware = $app->addErrorMiddleware(true,true,true);
+$middleware = $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $data = ['message'=> 'Hello world'];
+    $payload = json_encode($data);
+
+    $response->getBody()->write($payload);
+    return $response
+        ->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();

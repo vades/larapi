67192114050803165cbb802d0531cc0b9a9502b2
Martin Vach <martin.vach@mynxg.com>
2021-02-24 16:12:37 +0100

    Lumen

--- public/index.php
+++ public/index.php
@@ -1,17 +1,36 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+define ('JSON_UNESCAPED_SLASHES', 64);
+define ('JSON_UNESCAPED_UNICODE', 0);
+define ('JSON_PRESERVE_ZERO_FRACTION', 0);
+if (!defined('JSON_INVALID_UTF8_SUBSTITUTE')) {
+    //PHP < 7.2 Define it as 0 so it does nothing
+    define('JSON_INVALID_UTF8_SUBSTITUTE', 0);
+}
 
-require __DIR__ . '/../vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| First we need to get an application instance. This creates an instance
+| of the application / container and bootstraps the application so it
+| is ready to receive HTTP / Console requests from the environment.
+|
+*/
 
-$app = AppFactory::create();
-$middleware = $app->addErrorMiddleware(true,true,true);
+$app = require __DIR__.'/../lumen/bootstrap/app.php';
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
 $app->run();
